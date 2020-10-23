179d06ab9724d725d0c0769e8e827c6dfc0cc3b6
santiagojs <nevermindsjs@hotmail.com>
2020-10-23 13:46:50 -0300

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -93,7 +93,7 @@
 					              error_reporting(E_ALL);
 					              ini_set("display_errors", 1);
 					              // SDK de Mercado Pago
-					              require __DIR__ .  '/vendor/autoload.php';
+					              require 'vendor/autoload.php';
 
 					              MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
 					            //  MercadoPago\SDK::setPlatformId("PLATFORM_ID");
